391295b4e80e42e72227878d8d8f7438c0fdbb2f
Christian Berendt <berendt@osism.tech>
2021-07-11 12:12:04 +0200

    Push e.g. osism/openstackclient:5.5.0
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/push.sh
+++ scripts/push.sh
@@ -14,6 +14,17 @@ if [[ -n $DOCKER_REGISTRY ]]; then
     REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
 fi
 
+# push e.g. osism/openstackclient:wallaby
 docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$VERSION"
 docker push "$REPOSITORY:$VERSION"
 docker rmi "$REPOSITORY:$VERSION"
+
+# push e.g. osism/openstackclient:5.5.0
+version=$(docker run --rm "$REPOSITORY:$VERSION" openstack --version | awk '{ print $2 }')
+
+if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect "${REPOSITORY}:${version}" > /dev/null; then
+    echo "The image ${REPOSITORY}:${version} already exists."
+else
+    docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+    docker push "$REPOSITORY:$version"
+fi
